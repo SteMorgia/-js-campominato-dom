c177153a5ec00c9eae10c44deaed7a3e29f10359
Stefano Morgia <ste.morgia@gmail.com>
2022-07-14 15:03:27 +0200

    add: parte boonus implementata, esercizio del giorno in corso

--- js/main.js
+++ js/main.js
@@ -1,46 +1,62 @@
-const gridDom = document.getElementById('grid');
-let myArray = [];
-let livello = document.getElementById('livelli');
-let difficolta = livello.value;
+const playDom = document.getElementById('button');
 
-for (let i = 0; i < 100; i++) {
-    const divSquare = document.createElement('div');
-    gridDom.append(divSquare);
-    divSquare.classList.add('square');
-    console.log(i + 1);
-    divSquare.append(i + 1);
-}
+playDom.addEventListener("click", playGame);
 
-/*if (difficolta === "facile"){
-    creaQuadrati(100);
-    gridDom.classList.add('facile');
-} else if (difficolta === "medio") {
-    creaQuadrati(81);
-    gridDom.classList.add('medio');
-} else {
-    creaQuadrati(49);
-    gridDom.classList.add('difficile');
-}*/
+
+
+
+
+
+
+
     
 
 
 
 
+function playGame () {
+    const gridDom = document.getElementById('grid');
+    let myArray = [];
+    let livello = document.getElementById('livelli');
+    let difficolta = livello.value;
+    
 
+    if (difficolta === "facile"){
+        creaQuadrati(100);
+        gridDom.classList.add('facile');
+    } else if (difficolta === "medio") {
+        creaQuadrati(81);
+        gridDom.classList.add('medio');
+    } else {
+        creaQuadrati(49);
+        gridDom.classList.add('difficile');
+    }
 
-function generaNumeri (min, max) {
-    return (Math.round(Math.random(max - min) * 100));
-}
-
-
-
-function creaQuadrati(square_number) {
-    for (let i = 0; i < square_number; i++) {
-        const divSquare = document.createElement('div');
-        gridDom.append(divSquare);
-        divSquare.classList.add('square');
-        console.log(i + 1);
-        divSquare.append(i + 1);
+    function coloraCella () {
+        this.classList.toggle ("clicked");
+    }
+    
+    function generaBombe (min, max) {
+        for (let x = 0; x < 16; x++) {
+            return (Math.round(Math.random(max - min) * 100));
+        } 
+        console.log(generaBombe(1, 81));
+    }
+    
+    
+    
+    function creaQuadrati(square_number) {
+        for (let i = 0; i < square_number; i++) {
+            const divSquare = document.createElement('div');
+            divSquare.classList.add('square');
+            console.log(i + 1);
+            divSquare.append(i + 1);
+            divSquare.addEventListener("click", coloraCella );
+            gridDom.append(divSquare);
+        }
     }
 }
+
+
+
 
